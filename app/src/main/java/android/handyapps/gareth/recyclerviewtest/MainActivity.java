82fea21f6e5cc32603bfceb238e7ec862ce68f89
GariceZa <garethpriceza@gmail.com>
2015-01-02 14:26:46 +0200

    Created:
    - color.xml:
       - stores the color values for the swipe refresh view to cycle through while refreshing content
    
    Updated:
    
    - MainActivity.java:
    	- added swipe refreshing ability
    - activity_main.xml:
    	- added swipe refresh view to the layout

--- app/src/main/java/android/handyapps/gareth/recyclerviewtest/MainActivity.java
+++ app/src/main/java/android/handyapps/gareth/recyclerviewtest/MainActivity.java
@@ -2,6 +2,7 @@ package android.handyapps.gareth.recyclerviewtest;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,17 +23,18 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
+    SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     //private String [] myDataset = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setting up the recycler view
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
         // Use this setting to improve performance if you know that changes
@@ -43,64 +45,81 @@ public class MainActivity extends ActionBarActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        //--------------------------------------------------
 
+        // Executing async task to retrieve data from api
         new LoadAlerts().execute();
 
-    }
 
-private class LoadAlerts extends AsyncTask<Void,Void,String> {
-
-    ArrayList<String> time = new ArrayList<>();
-    ArrayList<String> msg = new ArrayList<>();
-
-    @Override
-    protected String doInBackground(Void... params) {
-        String resp = null;
-        String url = "http://animalalert.garethprice.co.za/selectAlerts.php";
-        HttpEntity httpEntity;
-
-        try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpGet httpGet = new HttpGet(url);
-            HttpResponse httpResponse = httpClient.execute(httpGet);
-            httpEntity = httpResponse.getEntity();
-
-            if (httpEntity != null) {
-                resp = EntityUtils.toString(httpEntity);
+        // Setting up the swipe refresh layout
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new LoadAlerts().execute();
             }
+        });
+        //---------------------------------------------------
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return resp;
     }
 
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-    Log.v("Response", s);
+    private class LoadAlerts extends AsyncTask<Void, Void, String> {
 
-    try{
-        JSONArray jsonArray = new JSONArray(s);
+        ArrayList<String> time = new ArrayList<>();
+        ArrayList<String> msg = new ArrayList<>();
 
-        for (int i = 0; i < jsonArray.length(); i++) {
+        @Override
+        protected String doInBackground(Void... params) {
+            String resp = null;
+            String url = "http://animalalert.garethprice.co.za/selectAlerts.php";
+            HttpEntity httpEntity;
 
             try {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                time.add(jsonObject.getString("AlertTime"));
-                msg.add(jsonObject.getString("AlertDescription"));
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url);
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                httpEntity = httpResponse.getEntity();
+
+                if (httpEntity != null) {
+                    resp = EntityUtils.toString(httpEntity);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return resp;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.v("Response", s);
+
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    try {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        time.add(jsonObject.getString("AlertTime"));
+                        msg.add(jsonObject.getString("AlertDescription"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        }
-    } catch (JSONException e) {
-        e.printStackTrace();
-    }
 
-    // Specify an adapter
-     mAdapter = new MyAdapter(time,msg);
-     mRecyclerView.setAdapter(mAdapter);
-}
-}
+            // Specify an adapter
+            mAdapter = new MyAdapter(time, msg);
+            // Set the adapter
+            mRecyclerView.setAdapter(mAdapter);
+            // Set swipe refresh to false if swiped
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
+    }
 }
